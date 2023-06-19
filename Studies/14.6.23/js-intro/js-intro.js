b2cc4f94b8d808873e58bf8f8b644a73814b53d5
Edo Mathias <edo.mathias@gmail.com>
2023-06-19 21:31:42 +0300

    js inro files

---
+++ Studies/14.6.23/js-intro/js-intro.js
@@ -0,0 +1,60 @@
+// document.write('Hello World<br>');
+
+// // Variable types
+
+// // Number
+// var myFirstVar = 123;
+// let a = 222;
+// let aa = 333;
+// const b = 333.55;
+
+// // String
+// let c = 'My string';
+
+// // Boolean
+// let d = true;
+// let e = false;
+
+// // input
+// // let p = prompt('please type your name', 'my name');
+// // document.write(p);
+
+// a = a + b;
+// a = a + aa;
+// a = 100 - 20;
+// a = 100 / 2;
+// console.log(a);
+
+// a = a * 10;
+// console.log(a);
+
+// a = a % 2;
+// console.log(a);
+
+// console.log(c);
+// c = `${c} from JavaScript`;
+// console.log(c);
+
+// let num = 5;
+// let str = 'string';
+// str = str + num;
+
+// let num1 = 5;
+// let num2 = 8;
+// let res = num1 + num2 + '';
+// console.log(res, typeof res);
+
+// let num3 = 5;
+// num3++;
+// console.log(num3);
+
+// num3 += 2;
+// console.log(num3);
+
+// num3--;
+// console.log(num3--);
+
+let num1 = Number(prompt('Enter the first number:'));
+let num2 = Number(prompt('Enter the second number:'));
+let sum = (num1 + num2) / 2;
+document.write(`<h1>The average is: ${sum}</h1>`);
