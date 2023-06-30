31131fabcf508c95c8107eceb03d3cb043ccea97
edo.mathias <edo.mathias@overwolf.com>
2023-06-30 18:46:35 +0300

    finished homework for DOM homework

--- Homework/dom-manipulations/index.js
+++ Homework/dom-manipulations/index.js
@@ -1,15 +1,15 @@
 // Question 1
 function addItemToElement(sourceElementId) {
   const element = document.getElementById(sourceElementId);
-  let listItem = prompt('add an item:');
+  let listItem = prompt("add an item:");
   let item = `<li>${listItem}</li>`;
   element.innerHTML += item;
 }
 
 // Question 2
 function changeOrder() {
-  const ulElement = document.getElementById('ul2');
-  const liElements = document.getElementsByClassName('li2');
+  const ulElement = document.getElementById("ul2");
+  const liElements = document.getElementsByClassName("li2");
   let liArray = [];
 
   //   push to liArray all li from liElements
@@ -26,7 +26,7 @@ function changeOrder() {
   }
 
   //   Restart the current li inside the ul
-  ulElement.innerHTML = '';
+  ulElement.innerHTML = "";
 
   //   create a loop that will add the li based on the shuffled
   // order from the previous loop
@@ -38,17 +38,17 @@ function changeOrder() {
 // Question 3
 function paintList() {
   // Get list 3 by ID
-  const ul3Element = document.getElementById('ul3');
-  const li3Elements = document.getElementsByClassName('li3');
+  const ul3Element = document.getElementById("ul3");
+  const li3Elements = document.getElementsByClassName("li3");
 
   // Iterate through the li items and paint
   // even lines in color 1 and uneven lines
   // in color 2
   for (let i = 0; i < li3Elements.length; i++) {
     if (i % 2 === 0) {
-      li3Elements[i].style.color = 'red';
+      li3Elements[i].style.color = "red";
     } else {
-      li3Elements[i].style.color = 'blue';
+      li3Elements[i].style.color = "blue";
     }
   }
 }
@@ -56,44 +56,51 @@ function paintList() {
 // Question 4
 const studentInformation = [
   {
-    firstName: 'Edo',
-    lastName: 'Mathias',
-    mail: 'Edo.Mathias@gmail.com',
+    "First Name": "Edo",
+    "Last Name": "Mathias",
+    Mail: "Edo.Mathias@gmail.com",
   },
   {
-    firstName: 'Edo',
-    lastName: 'Mathias',
-    mail: 'Edo.Mathias@gmail.com',
+    "First Name": "Edo",
+    "Last Name": "Mathias",
+    Mail: "Edo.Mathias@gmail.com",
   },
   {
-    firstName: 'Edo',
-    lastName: 'Mathias',
-    mail: 'Edo.Mathias@gmail.com',
-  },
-  {
-    firstName: 'Edo',
-    lastName: 'Mathias',
-    mail: 'Edo.Mathias@gmail.com',
+    "First Name": "Edo",
+    "Last Name": "Mathias",
+    Mail: "Edo.Mathias@gmail.com",
   },
 ];
 
 function createTable(array) {
-  const table = document.getElementById('table');
-  const headersRow = document.getElementById('headers-row');
-  const tableBody = document.getElementById('tbody');
-  for (obj of array) {
-    for (generalInfo in obj) {
-      headersRow.innerHTML += `<th>${generalInfo}</th>`;
-    }
-    break;
+  const table = document.getElementById("table");
+  let tableHTML = "<thead style='border: 1px solid black'><tr>";
+
+  // Create table headers
+  for (generalInfo in array[0]) {
+    tableHTML += `<th style='border: 1px solid black'>${generalInfo}</th>`;
   }
+
+  tableHTML += "</tr></thead><tbody>";
+
+  // Create table rows
   for (obj of array) {
-    for (information in obj) {
-      const row = document.querySelector('tbody > tr');
-      row.innerHTML += `<td>${obj[information]}</td>`;
+    let count = 0;
+    tableHTML += "<tr>";
+
+    for (info in obj) {
+      if (count < 3) {
+        tableHTML += `<td style='border: 1px solid black'>${obj[info]}</td>`;
+        count++;
+      }
     }
-    tableBody.innerHTML += `<tr></tr>`;
+
+    tableHTML += "</tr>";
   }
+
+  tableHTML += "</tbody>";
+
+  table.innerHTML += tableHTML;
 }
 
 createTable(studentInformation);
