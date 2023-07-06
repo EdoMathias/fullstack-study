a96dd198ee4cb89eb20f94624b783fc3be37d5db
Edo Mathias <edo.mathias@gmail.com>
2023-07-06 10:21:47 +0300

    added submitTask and createTaskCard functions added minor css for cards

--- Projects/project1/project.js
+++ Projects/project1/project.js
@@ -1,8 +1,37 @@
+let tasks = [];
+const taskForm = document.forms['task-board-form'];
+
 // function for submitTask()
-function submitTask() {}
+function submitTask() {
+  const taskInfo = taskForm['form-text-area'].value;
+  const dueDate = taskForm['form-date'].value;
+  const dueTime = taskForm['form-time'].value;
+  console.log(taskInfo);
+  console.log(dueDate);
+  console.log(dueTime);
+
+  tasks[tasks.length] = {
+    taskInfo: taskInfo,
+    dueDate: dueDate,
+    dueTime: dueTime,
+  };
+  console.log(tasks);
+  createTaskCard();
+}
 
 // function for creating a task card from an object
-function createTask(obj) {}
+function createTaskCard() {
+  document.querySelector('#open-task-container').innerHTML = '';
+  for (let i = 0; i < tasks.length; i++) {
+    document.querySelector('#open-task-container').innerHTML += `
+      <div class='task-card'>
+        <p>${tasks[i].taskInfo}</p>
+        <p>${tasks[i].dueDate}</p>
+        <p>${tasks[i].dueTime}</p>
+      </div>
+      `;
+  }
+}
 
 // function for deleteing a task
 function deleteTask() {}
