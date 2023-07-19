a687c03f043512b85d6b033c96ec89377880828c
Edo Mathias <edo.mathias@gmail.com>
2023-07-19 19:01:40 +0300

    Added fade-in functionality in project 1

--- Projects/project1/project-bootstrap.js
+++ Projects/project1/project-bootstrap.js
@@ -1,26 +1,56 @@
 let tasks = [];
-const taskForm = document.forms["task-board-form"];
+const taskForm = document.forms['task-board-form'];
 
 // function for submitTask()
 function submitTask() {
-  const taskInfo = taskForm["task-info"].value;
-  const dueDate = taskForm["due-date"].value;
-  const dueTime = taskForm["due-time"].value;
+  const taskInfo = taskForm['task-info'].value;
+  const dueDate = taskForm['due-date'].value;
+  const dueTime = taskForm['due-time'].value;
 
   tasks[tasks.length] = {
     taskInfo: taskInfo,
     dueDate: dueDate,
     dueTime: dueTime,
   };
-  createTaskCard();
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  createTaskCardWithFade();
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // function for creating a task card from an object
 function createTaskCard() {
-  document.querySelector("#open-task-container").innerHTML = "";
+  document.querySelector('#open-task-container').innerHTML = '';
   for (let i = 0; i < tasks.length; i++) {
-    document.querySelector("#open-task-container").innerHTML += `
+    document.querySelector('#open-task-container').innerHTML += `
+    <div class="task-card">
+    <div class="row">
+      <div class="col-12">
+        <button class="delete-button btn focus" onclick="deleteTask(${i})">
+          <i class="bi bi-trash-fill fs-5"></i>
+        </button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="task-info-container">
+          <p class="taskInfo larger-text">${tasks[i].taskInfo}</p>
+        </div>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-12">
+        <p class="taskDate mb-0">${tasks[i].dueDate}</p>
+        <p class="taskTime">${tasks[i].dueTime}</p>
+      </div>
+    </div>
+  </div>
+  `;
+  }
+}
+// function for creating a task card from an object
+function createTaskCardWithFade() {
+  document.querySelector('#open-task-container').innerHTML = '';
+  for (let i = 0; i < tasks.length; i++) {
+    document.querySelector('#open-task-container').innerHTML += `
     <div class="task-card fade-in">
     <div class="row">
       <div class="col-12">
@@ -57,12 +87,12 @@ function deleteTask(taskCardIndex) {
   }
   tasks = newTaskCards;
   createTaskCard();
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // function for loading and re-setting local storage
 function loadAndResetKey() {
-  let tasksFromLocalStorage = localStorage.getItem("tasks");
+  let tasksFromLocalStorage = localStorage.getItem('tasks');
   if (tasksFromLocalStorage) {
     tasks = JSON.parse(tasksFromLocalStorage);
   } else {
