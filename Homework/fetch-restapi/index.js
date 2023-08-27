a45457117841f9c3a36dfaaf755df5871149835b
Edo Mathias <edo.mathias@gmail.com>
2023-08-28 00:29:17 +0300

    added fetch-restapi homework

--- Homework/fetch-restapi/index.js
+++ Homework/fetch-restapi/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+let users = [];
+
+(async function () {
+  users = await service.getAllUsers();
+})();
+
 async function init() {
   try {
     const users = await service.getAllUsers();
@@ -20,8 +26,40 @@ function generateTableBody(array) {
         <td>${user.name.lastname}</td>
         <td>${user.email}</td>
         <td>${user.phone}</td>
+        <td><button onclick="getDeletedUser(${user.id}); deleteUser(${user.id})">X</td>
+        <td><button onclick="getUserInfo(${user.id});">Get Info</td>
     </tr>`
     )
     .join('');
   tbody.innerHTML = arrBodyData;
+}
+
+async function getDeletedUser(userId) {
+  try {
+    const data = await service.deleteUser(userId);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deleteUser(userId) {
+  // const userToDelete = getDeletedUser(userId);
+  users = users.filter((user) => user.id !== userId);
+  generateTableBody(users);
+}
+
+async function getUserInfo(userId) {
+  try {
+    const user = await service.getUserInfo(userId);
+    alert(`
+  First Name: ${user.name.firstname}
+  Last Name: ${user.name.lastname}
+  Email: ${user.email}
+  Phone Number: ${user.phone}
+  City: ${user.address.city}
+  `);
+  } catch (error) {
+    console.log(error);
+  }
 }
--- Homework/fetch-restapi/service.js
+++ Homework/fetch-restapi/service.js
@@ -16,13 +16,25 @@ const service = (() => {
     const response = await fetch(`${baseUrl}/users/${userId}`, {
       method: 'DELETE',
     });
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+    throw new Error(`Failed to delete user number: ${userId}`);
   }
 
   async function getUserInfo(userId) {
     const response = await fetch(`${baseUrl}/users/${userId}`);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+    throw new Error(`Failed to get User's info`);
   }
 
   return {
     getAllUsers,
+    deleteUser,
+    getUserInfo,
   };
 })();
