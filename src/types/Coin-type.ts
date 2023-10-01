9213778b28d0e96123087bfa33efd28cc35a3478
Edo Mathias <edo.mathias@gmail.com>
2023-10-01 17:17:59 +0300

    fix files created in main folder by mistake

--- src/types/Coin-type.ts
+++
@@ -1,7 +0,0 @@
-export type Coin = { id: string; symbol: string; name: string };
-export type CoinMarketData = {
-  market_data: {
-    current_price: { usd: number; eur: number; ils: number };
-  };
-  image: { small: string };
-};
