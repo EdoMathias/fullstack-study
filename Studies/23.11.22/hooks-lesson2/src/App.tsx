ccbaf0a672caa3c226dc90d74e24852d79199f3d
Edo Mathias <edo.mathias@gmail.com>
2023-11-22 20:22:13 +0200

    useEffect lesson

---
+++ Studies/23.11.22/hooks-lesson2/src/App.tsx
@@ -0,0 +1,15 @@
+import { Timer } from './component/timer/Timer';
+import './App.css';
+import { Products } from './component/products/Products';
+
+function App() {
+  return (
+    <>
+      <Timer />
+      <hr></hr>
+      <Products />
+    </>
+  );
+}
+
+export default App;
