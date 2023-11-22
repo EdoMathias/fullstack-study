e52d389c381a5f48b4aad4aa8eab5a8418f39b75
Edo Mathias <edo.mathias@gmail.com>
2023-11-22 12:23:36 +0200

    format homework

--- React-Homeworks/useState-homework/src/App.tsx
+++ React-Homeworks/useState-homework/src/App.tsx
@@ -1,15 +1,12 @@
-
-import './App.css'
-import { FormData } from './components/FormData/FormData'
+import './App.css';
+import { FormData } from './components/FormData/FormData';
 
 function App() {
-  
-
   return (
     <>
-      <FormData firstName='Edo' lastName='Mathias' age='23'/>
+      <FormData firstName="Edo" lastName="Mathias" age="23" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
