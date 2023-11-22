3d4bfce6df86f552d988733a5b68d907531491ac
Edo Mathias <edo.mathias@gmail.com>
2023-11-22 21:27:22 +0200

    Init online-shop class project

--- recipe-site-project/recipe-site/src/main.tsx
+++ recipe-site-project/recipe-site/src/main.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 // import './index.css';
-import { SiteHeader } from './components/site-header/SiteHeader.tsx';
-import { Footer } from './components/footer/Footer.tsx';
-import { NavMenu } from './components/navigation-menu/NavMenu.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
