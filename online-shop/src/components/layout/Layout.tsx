023183f6d9f36eedc0f90694f483d3f74e9484fc
Edo Mathias <edo.mathias@gmail.com>
2023-12-03 19:17:38 +0200

    Used the header in the layout component, added outlet component for the router

--- online-shop/src/components/layout/Layout.tsx
+++ online-shop/src/components/layout/Layout.tsx
@@ -1,14 +1,18 @@
+import { Outlet } from 'react-router-dom';
 import { SideNavigation } from '../side-navigation/SideNavigation';
 import style from './layout.module.css';
+import { Header } from '../header/Header';
 
 export const Layout = () => {
   return (
     <div className={style.layout}>
-      <div className={style.header}>Header</div>
+      <div className={style.header}>
+        <Header />
+      </div>
       <div className={style.sidebar}>
         <SideNavigation />
       </div>
-      <div className={style.main}>Main</div>
+      <div className={style.main}>{<Outlet />}</div>
       <div className={style.footer}>Footer</div>
     </div>
   );
