39f4a03de676a5391fd2925cdd81eec8e71fc300
Edo Mathias <edo.mathias@gmail.com>
2023-12-03 19:59:25 +0200

    Added category routing mechanism

--- online-shop/src/pages/category/Category.tsx
+++ online-shop/src/pages/category/Category.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Product } from '../../types/types';
+import { getProductsByCategoryId } from '../../services/products-service';
 
 export const Category = () => {
   const { categoryId } = useParams();
+  const [products, setProducts] = useState<Product[] | null>(null);
 
-  return <div>Category: {categoryId}</div>;
+  useEffect(() => {
+    if (categoryId) {
+      const callGetProductsByCategoryId = async () => {
+        const result = await getProductsByCategoryId(categoryId);
+        setProducts(result!);
+      };
+      callGetProductsByCategoryId();
+    }
+  }, [categoryId]);
+
+  return products === null ? (
+    <div>LOADING...</div>
+  ) : (
+    <div>
+      Category: {categoryId}:
+      <ul>
+        {products?.map((product) => (
+          <li key={product.title}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
--- online-shop/src/types/types.ts
+++ online-shop/src/types/types.ts
@@ -2,3 +2,13 @@ export type Category = {
   title: string;
   id: string;
 };
+
+export type Product = {
+  id: string;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  categoryId: string;
+  image: string;
+};
