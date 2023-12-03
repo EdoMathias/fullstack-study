ed119771056c5a0aefaea2c612d707a425912990
Edo Mathias <edo.mathias@gmail.com>
2023-12-03 18:26:14 +0200

    Added products service for better file structure

---
+++ online-shop/src/services/products-service.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { Category } from '../types/types';
+
+const getCategoriesUrl = 'http://localhost:3000/categories';
+
+export const getCategories = async () => {
+  try {
+    const result = await axios.get<Category[]>(getCategoriesUrl);
+    const categories = result.data;
+    return categories;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        'Failed fetching categories' + error.response?.statusText
+      );
+    }
+  }
+};
