3ae7a9aca521e6b4a174c4adf400a6cdd1b81cc1
Edo Mathias <edo.mathias@gmail.com>
2023-12-03 19:17:59 +0200

    Added router mechanism using react router

--- online-shop/src/main.tsx
+++ online-shop/src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import { Layout } from './components/layout/Layout.tsx';
+import { Home } from './components/home/Home.tsx';
+import { About } from './components/about/About.tsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="home" element={<Home />}></Route>
+      <Route path="about" element={<About />}></Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+    {/* <App /> */}
+  </React.StrictMode>
+);
