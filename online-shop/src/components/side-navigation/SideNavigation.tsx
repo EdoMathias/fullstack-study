101d90df1798d2e2d972a27ea64ccefbc1b5b640
Edo Mathias <edo.mathias@gmail.com>
2023-12-03 19:59:12 +0200

    Fix import on side-navigation

--- online-shop/src/components/side-navigation/SideNavigation.tsx
+++ online-shop/src/components/side-navigation/SideNavigation.tsx
@@ -1,7 +1,7 @@
 // import { Link } from 'react-router-dom'; // If you're using React Router
 import { useEffect, useState } from 'react';
 import { Category } from '../../types/types';
-import { getCategories } from '../../services/products-service';
+import { getCategories } from '../../services/categories-service';
 import style from './sidebar.module.css';
 import { Link } from 'react-router-dom';
 
