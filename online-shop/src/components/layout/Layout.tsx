a5cb3e303250cca0328386104ec3580c83983e00
Edo Mathias <edo.mathias@gmail.com>
2023-12-06 20:18:10 +0200

    Init restructure

--- online-shop/src/components/layout/Layout.tsx
+++ online-shop/src/components/layout/Layout.tsx
@@ -5,24 +5,16 @@ import { Header } from '../header/Header';
 
 //
 
-import * as React from 'react';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import Button from '@mui/material/Button';
+import { styled } from '@mui/system';
 
-const drawerWidth = 240;
+const MyButton = styled('button')({
+  color: 'pink',
+  padding: '50px',
+});
 
 export const Layout = () => {
   // return (
@@ -41,61 +33,19 @@ export const Layout = () => {
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Edo's Online Store
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-      >
-        <Toolbar />
-        <Box sx={{ overflow: 'auto' }}>
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      </Drawer>
+      <Header />
+      <SideNavigation />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
-        <Button variant="contained" color="error" size="large">
+        <Button
+          variant="contained"
+          color="error"
+          size="large"
+          sx={{ padding: '50px' }}
+        >
           Button 1
         </Button>
+        <MyButton>Button 2</MyButton>
         {/* <Typography paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
--- online-shop/src/components/side-navigation/SideNavigation.tsx
+++ online-shop/src/components/side-navigation/SideNavigation.tsx
@@ -4,6 +4,19 @@ import { Category } from '../../types/types';
 import { getCategories } from '../../services/categories-service';
 import style from './sidebar.module.css';
 import { Link } from 'react-router-dom';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+const drawerWidth = 240;
 
 export const SideNavigation = () => {
   const [categories, setcategories] = useState<Category[] | null>(null);
@@ -16,17 +29,60 @@ export const SideNavigation = () => {
     callGetAllCategories();
   }, []);
 
-  return categories === null ? (
-    <div>LOADING...</div>
-  ) : (
-    <nav className={style.sidebar}>
-      <ul className={style.menu}>
-        {categories?.map((category) => (
-          <li key={category.id}>
-            <Link to={`category/${category.id}`}>{category.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+  // return categories === null ? (
+  //   <div>LOADING...</div>
+  // ) : (
+  //   <nav className={style.sidebar}>
+  //     <ul className={style.menu}>
+  //       {categories?.map((category) => (
+  //         <li key={category.id}>
+  //           <Link to={`category/${category.id}`}>{category.title}</Link>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   </nav>
+  // );
+
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+    >
+      <Toolbar />
+      <Box sx={{ overflow: 'auto' }}>
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
   );
 };
