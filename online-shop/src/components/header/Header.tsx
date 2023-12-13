183d9dc022b6ceb06ecf40561e0b1e0927cd0388
Edo Mathias <edo.mathias@gmail.com>
2023-12-13 19:17:34 +0200

    Added header design with cart icon and badge count

--- online-shop/src/components/header/Header.tsx
+++ online-shop/src/components/header/Header.tsx
@@ -1,10 +1,20 @@
+import { Badge, Button, IconButton } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Grid from '@mui/material/Grid';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useAppSelecetor } from '../../app/hooks';
 
 export const Header = () => {
+  const cartProductsAmount = useAppSelecetor((state) => {
+    let totalProducts = 0;
+    Object.keys(state.card.products).forEach(
+      (key) => (totalProducts += state.card.products[key].amount)
+    );
+    return totalProducts;
+  });
   return (
     <AppBar
       position="fixed"
@@ -18,11 +28,22 @@ export const Header = () => {
             </Typography>
           </Grid>
           <Grid item>
+            <Link to={'/cart'} style={{ margin: '0 10px' }}>
+              <IconButton>
+                <Badge badgeContent={cartProductsAmount}>
+                  <ShoppingCartIcon />
+                </Badge>
+              </IconButton>
+            </Link>
             <Link to={'/'} style={{ margin: '0 10px' }}>
-              Home
+              <Button variant="contained" color="success">
+                Home
+              </Button>
             </Link>
             <Link to={'/about'} style={{ margin: '0 10px' }}>
-              About
+              <Button variant="contained" color="success">
+                About
+              </Button>
             </Link>
           </Grid>
         </Grid>
