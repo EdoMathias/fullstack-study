8f9264dcd8433e15f2d0498b89b338213d6cdc1a
EdoMathias <edo.mathias@overwolf.com>
2023-12-17 20:56:31 +0200

    Changes url variable to a better name, Added createProduct fucntion

--- online-shop/src/services/singleProduct-service.ts
+++ online-shop/src/services/singleProduct-service.ts
@@ -1,18 +1,27 @@
 import axios from 'axios';
 import { Product } from '../types/types';
 
-const getSingleProductUrl = 'http://localhost:3000/products';
+const productsUrl = 'http://localhost:3000/products';
 
 export const getSingleProduct = async (productId: string) => {
   try {
-    const result = await axios.get<Product[]>(
-      `${getSingleProductUrl}?id=${productId}`
-    );
+    const result = await axios.get<Product[]>(`${productsUrl}?id=${productId}`);
     const product = result.data;
     return product;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       throw new Error('Failed fetching product' + error.response?.statusText);
     }
+  }
+};
+
+export const createProduct = async (product: Product) => {
+  try {
+    await axios.post(productsUrl, product);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.statusText);
+    }
+    throw error;
   }
 };
