194b5e63497549da5bfe42a48fe241a9817b0110
EdoMathias <edo.mathias@overwolf.com>
2023-12-17 20:31:18 +0200

    Added default cache time

--- online-shop/src/main.tsx
+++ online-shop/src/main.tsx
@@ -7,7 +7,9 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 1000 * 20 } },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
