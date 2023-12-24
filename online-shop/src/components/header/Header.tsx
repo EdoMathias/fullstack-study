d5a6650226448a832f7186c5ffac950376c38bc6
Edo Mathias <edo.mathias@gmail.com>
2023-12-24 18:07:47 +0200

    Edit header to have avatar

--- online-shop/src/components/header/Header.tsx
+++ online-shop/src/components/header/Header.tsx
@@ -1,13 +1,45 @@
-import { Badge, Button, IconButton } from '@mui/material';
+import {
+  Avatar,
+  Badge,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Tooltip,
+} from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Grid from '@mui/material/Grid';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useAppSelecetor } from '../../app/hooks';
+import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../features/auth-slice';
+import { signOut } from '../../services/auth-service';
+import { grey } from '@mui/material/colors';
 
 export const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogOut = () => {
+    dispatch(logout());
+    signOut();
+    navigate('/');
+  };
+
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   const cartProductsAmount = useAppSelecetor((state) => {
     let totalProducts = 0;
     Object.keys(state.card.products).forEach(
@@ -40,11 +72,6 @@ export const Header = () => {
                 Home
               </Button>
             </Link>
-            <Link to={'/about'} style={{ margin: '0 10px' }}>
-              <Button variant="contained" color="success">
-                About
-              </Button>
-            </Link>
             <Link to={'/product/create'} style={{ margin: '0 10px' }}>
               <Button variant="contained" color="success">
                 Create Product
@@ -55,6 +82,38 @@ export const Header = () => {
                 Login
               </Button>
             </Link>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar>
+                  <AccountCircleIcon />
+                </Avatar>
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem key={'account'} onClick={handleCloseUserMenu}>
+                <Link to={'/account'} style={{ textDecoration: 'none' }}>
+                  <Typography textAlign="center">Account</Typography>
+                </Link>
+              </MenuItem>
+              <MenuItem key={'logout'} onClick={handleLogOut}>
+                <Typography textAlign="center">Logout</Typography>
+              </MenuItem>
+            </Menu>
           </Grid>
         </Grid>
       </Toolbar>
