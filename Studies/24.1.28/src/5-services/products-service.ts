1bc8a6a0d651e5b513e6ebb54fb8aacfff889525
Edo Mathias <edo.mathias@gmail.com>
2024-01-31 18:26:13 +0200

    Added get product by id and init add product

--- Studies/24.1.28/src/5-services/products-service.ts
+++ Studies/24.1.28/src/5-services/products-service.ts
@@ -1,3 +1,4 @@
+import { OkPacketParams } from 'mysql2';
 import { dal } from '../2-utils/dal';
 import { ProductModel } from '../3-models/product-model';
 
@@ -6,6 +7,30 @@ class ProductsService {
     const sql = 'SELECT * FROM products';
     const products = await dal.exceute(sql);
     return products;
+  }
+
+  public async getProductById(id: number): Promise<ProductModel> {
+    const sql = 'SELECT * FROM products WHERE id = ' + id;
+
+    // dal.execute always returns an array from the DB
+    const products = await dal.exceute(sql);
+
+    // Extract the single product
+    const product = products[0];
+
+    return product;
+  }
+
+  // Add new product:
+  public async addProduct(product: ProductModel): Promise<ProductModel> {
+    const sql = `INSERT INTO products(name, price, stock) 
+      VALUES('${product.name}', ${product.price}, ${product.stock})`;
+    const info: OkPacketParams = await dal.exceute(sql);
+
+    // Set back the auto incremented id to the product
+    product.id = info.insertId;
+
+    return product;
   }
 }
 
--- Studies/24.1.28/src/6-controllers/products-controller.ts
+++ Studies/24.1.28/src/6-controllers/products-controller.ts
@@ -12,6 +12,7 @@ class ProductsController {
   // Registering routes
   private registerRoutes(): void {
     this.router.get('/api/products', this.getAllProducts);
+    this.router.get('/api/products/:id', this.getProductById);
   }
 
   // GET all products
@@ -21,6 +22,25 @@ class ProductsController {
   ): Promise<void> {
     const products = await productsService.getAllProducts();
     response.json(products);
+  }
+
+  // GET single product
+  private async getProductById(
+    request: Request,
+    response: Response
+  ): Promise<void> {
+    const id = request.params.id;
+    const product = await productsService.getProductById(Number(id));
+    response.json(product);
+  }
+
+  // Add product
+  private async addProduct(
+    request: Request,
+    response: Response
+  ): Promise<void> {
+    const product = request.body;
+    // We must tell express to create this "body" from the given json.
   }
 }
 
