cb9c5f3f1c850f89445dab45c3a194f9cdd4b766
Edo Mathias <edo.mathias@gmail.com>
2024-01-31 21:02:30 +0200

    Added test for the error middleware

--- Studies/24.1.28/src/4-middleware/logging-middleware.ts
+++ Studies/24.1.28/src/4-middleware/logging-middleware.ts
@@ -6,6 +6,16 @@ class LoggingMiddleware {
     response: Response,
     next: NextFunction
   ): void {
+    // Test for next with an error message when failing on a case:
+    // if (Math.random() < 0.25) {
+    //   const err = {
+    //     status: 500,
+    //     message: 'Testing catchAll middleware',
+    //   };
+    //   next(err);
+    //   return;
+    // }
+
     console.log(`Request: ${request.method}`);
     console.log(`Route: ${request.route}`);
     console.log(`Body: ${JSON.stringify(request.body)}`);
