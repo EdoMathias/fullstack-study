543058c653d25f005776da00dd8c9a7990dc034f
edo.mathias <edo.mathias@overwolf.com>
2024-02-16 17:31:08 +0200

    Added login api

---
+++ Studies/24.1.28/src/3-models/credentials-model.ts
@@ -0,0 +1,13 @@
+import { RoleModel } from './role-model';
+
+export class CredentialsModel {
+  
+  public email: string;
+  public password: string;
+
+  constructor(credentials: CredentialsModel) {
+    this.email = credentials.email;
+    this.password = credentials.password;
+  }
+
+}
--- Studies/24.1.28/src/5-services/auth-service.ts
+++ Studies/24.1.28/src/5-services/auth-service.ts
@@ -3,6 +3,8 @@ import { dal } from '../2-utils/dal';
 import { UserModel } from '../3-models/user-model';
 import { RoleModel } from '../3-models/role-model';
 import { cyber } from '../2-utils/cyber';
+import { CredentialsModel } from '../3-models/credentials-model';
+import { UnauthorizedError } from '../3-models/client-errors';
 
 class AuthService {
   public async register(user: UserModel): Promise<string> {
@@ -16,6 +18,28 @@ class AuthService {
 
     // Set back id:
     user.id = info.insertId;
+
+    const token = cyber.getNewToken(user);
+
+
+    // Return user
+    return token;
+  }
+
+
+  public async login(credentials: CredentialsModel): Promise<string> {
+
+    // Create sql: 
+    const sql = `SELECT * FROM users WHERE
+     email = '${credentials.email}' AND password = '${credentials.password}'`;
+
+    const users = await dal.exceute(sql);
+
+    const user = users[0];
+
+    if (!user) {
+      throw new UnauthorizedError("Incorrect email or password")
+    }
 
     const token = cyber.getNewToken(user);
 
