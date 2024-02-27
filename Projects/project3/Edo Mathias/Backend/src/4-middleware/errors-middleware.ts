72a64e521f8d2d3bb836cfebec8ec0e449194c5d
Edo Mathias <edo.mathias@gmail.com>
2024-02-27 12:14:48 +0200

    Added auth service and controllers, changed enums file name

--- Projects/project3/Edo Mathias/Backend/src/4-middleware/errors-middleware.ts
+++ Projects/project3/Edo Mathias/Backend/src/4-middleware/errors-middleware.ts
@@ -1,40 +1,48 @@
-import { NextFunction, Request, Response } from "express";
-import { StatusCode } from "../3-models/enums";
-import { RouteNotFoundError } from "../3-models/client-errors";
-import { logger } from "../2-utils/logger";
-import { appConfig } from "../2-utils/app-config";
+import { NextFunction, Request, Response } from 'express';
+import { StatusCode } from '../3-models/status-codes';
+import { RouteNotFoundError } from '../3-models/client-errors';
+import { logger } from '../2-utils/logger';
+import { appConfig } from '../2-utils/app-config';
 
 class ErrorsMiddleware {
+  // Route not found:
+  public routeNotFound(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): void {
+    // Create client error:
+    const err = new RouteNotFoundError(request.originalUrl);
 
-    // Route not found:
-    public routeNotFound(request: Request, response: Response, next: NextFunction): void {
+    // Go to catch-all:
+    next(err);
+  }
 
-        // Create client error:
-        const err = new RouteNotFoundError(request.originalUrl);
+  // Catch all:
+  public catchAll(
+    err: any,
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): void {
+    // Log error to console:
+    console.log(err);
 
-        // Go to catch-all: 
-        next(err);
-    }
-    
-    // Catch all: 
-    public catchAll(err: any, request: Request, response: Response, next: NextFunction): void {
+    // Log error to file:
+    logger.logError(err);
 
-        // Log error to console:
-        console.log(err);
+    // Take error status:
+    const status = err.status || StatusCode.InternalServerError;
 
-        // Log error to file:
-        logger.logError(err);
-        
-        // Take error status: 
-        const status = err.status || StatusCode.InternalServerError;
+    // Take error message:
+    const message =
+      status === StatusCode.InternalServerError && appConfig.isProduction
+        ? 'Some error, please try again later.'
+        : err.message;
 
-        // Take error message: 
-        const message = (status === StatusCode.InternalServerError && appConfig.isProduction) ? "Some error, please try again later." : err.message;
-
-        // Response the error:
-        response.status(status).send(message);
-    }
-
+    // Response the error:
+    response.status(status).send(message);
+  }
 }
 
 export const errorsMiddleware = new ErrorsMiddleware();
