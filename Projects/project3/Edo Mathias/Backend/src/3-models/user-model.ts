d60e2bf15d9f0499c8d7a8dea3866d6cbdd26a30
Edo Mathias <edo.mathias@gmail.com>
2024-02-27 12:01:24 +0200

    updated some user and credentials validations

--- Projects/project3/Edo Mathias/Backend/src/3-models/user-model.ts
+++ Projects/project3/Edo Mathias/Backend/src/3-models/user-model.ts
@@ -23,9 +23,9 @@ export class UserModel {
   private static registerValidationSchema = Joi.object({
     id: Joi.number().forbidden(),
     firstName: Joi.string().required().min(2).max(50),
-    lastName: Joi.string().required().min(0).max(1000),
+    lastName: Joi.string().required().min(0).max(100),
     email: Joi.string().required().min(0).max(1000).email(),
-    password: Joi.string().required().min(8).max(48),
+    password: Joi.string().required().min(4).max(48),
     roleId: Joi.number().integer(),
   });
 
