bb56a43782bb61c059d7d57833b4e5fc25d3d828
Edo Mathias <edo.mathias@gmail.com>
2024-03-09 23:06:33 +0200

    Made improvements to the auth menu

--- Projects/project3/Edo Mathias/Frontend/src/Components/AuthArea/AuthMenu/AuthMenu.tsx
+++ Projects/project3/Edo Mathias/Frontend/src/Components/AuthArea/AuthMenu/AuthMenu.tsx
@@ -1,37 +1,25 @@
-import { useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
-import UserModel from "../../../Models/UserModel";
-import { AppState } from "../../../Redux/AppState";
-import { authService } from "../../../Services/AuthService";
-import "./AuthMenu.css";
-import { notify } from "../../../Utils/Notify";
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import UserModel from '../../../Models/UserModel';
+import { AppState } from '../../../Redux/AppState';
+import './AuthMenu.css';
+import LoginIcon from '@mui/icons-material/Login';
+import AuthAvatar from '../AuthAvatar/AuthAvatar';
 
 function AuthMenu(): JSX.Element {
+  const user = useSelector<AppState, UserModel>((appState) => appState.user);
 
-    const user = useSelector<AppState, UserModel>(appState => appState.user);
+  if (user) {
+    return <AuthAvatar user={user} />;
+  }
 
-    function logMeOut(): void {
-        notify.success(`Bye bye ${user.firstName}...`);
-        authService.logout();
-    }
-
-    if (user) {
-        return (
-            <div className="AuthMenu">
-                <span>Hello {user.firstName} {user.lastName} | </span>
-                <NavLink to="/list" onClick={logMeOut}>Logout</NavLink>
-            </div>
-        );
-    }
-
-    return (
-        <div className="AuthMenu">
-            <span>Hello Guest | </span>
-            <NavLink to="/login">Login</NavLink>
-            <span> | </span>
-            <NavLink to="/register">Register</NavLink>
-        </div>
-    );
+  return (
+    <div className="AuthMenu">
+      <NavLink to="/login">
+        <LoginIcon />
+      </NavLink>
+    </div>
+  );
 }
 
 export default AuthMenu;
--- Projects/project3/Edo Mathias/Frontend/src/Components/LayoutArea/Header/Header.tsx
+++ Projects/project3/Edo Mathias/Frontend/src/Components/LayoutArea/Header/Header.tsx
@@ -1,15 +1,15 @@
-import AuthMenu from "../../AuthArea/AuthMenu/AuthMenu";
-import logo from './EldenTrips.png'
-import "./Header.css";
+import AuthMenu from '../../AuthArea/AuthMenu/AuthMenu';
+import logo from './EldenTrips.png';
+import './Header.css';
 
 function Header(): JSX.Element {
-    return (
-        <div className="Header">
-            <img src={logo} width={75} />
-            <h1>Elden Trips</h1>
-            <AuthMenu />
-        </div>
-    );
+  return (
+    <div className="Header">
+      <img src={logo} />
+      <h1>Elden Trips</h1>
+      <AuthMenu />
+    </div>
+  );
 }
 
 export default Header;
