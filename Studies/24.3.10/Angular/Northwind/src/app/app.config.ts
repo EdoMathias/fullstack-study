d53a62b3cac53919ac113a10c8f690a9e4da243d
Edo Mathias <edo.mathias@gmail.com>
2024-03-13 19:29:49 +0200

    Replace axios with a built in HTTP library

--- Studies/24.3.10/Angular/Northwind/src/app/app.config.ts
+++ Studies/24.3.10/Angular/Northwind/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig = {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), provideHttpClient()],
 
   productsUrl: 'http://localhost:3030/api/products/',
 };
--- Studies/24.3.10/Angular/Northwind/src/app/services/products.service.ts
+++ Studies/24.3.10/Angular/Northwind/src/app/services/products.service.ts
@@ -1,16 +1,21 @@
-import axios from 'axios';
 import { ProductModel } from '../models/product.model';
 import { appConfig } from '../app.config';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   // Where to create an object
   providedIn: 'root', // root = create a single object for entire app
 })
 export class ProductsService {
+  public constructor(private http: HttpClient) {}
+
   public async getAllProducts(): Promise<ProductModel[]> {
-    const response = await axios.get<ProductModel[]>(appConfig.productsUrl);
-    const products = response.data;
+    // const response = await axios.get<ProductModel[]>(appConfig.productsUrl);
+    // const products = response.data;
+
+    const observable = this.http.get<ProductModel[]>(appConfig.productsUrl);
+    const products = await observable.toPromise();
     return products;
   }
 }
