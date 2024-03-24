a753d7959b2932c96c643006bfa30141aa1027ac
Edo Mathias <edo.mathias@gmail.com>
2024-03-24 18:40:53 +0200

    Added a liking system functionality to the backend

---
+++ Projects/project3/Edo Mathias/Backend/src/6-controllers/likes-controller.ts
@@ -0,0 +1,48 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { LikeModel } from '../3-models/like-model';
+import { StatusCode } from '../3-models/status-codes';
+import { likesService } from '../5-services/likes-service';
+
+class LikesController {
+  public readonly router = express.Router();
+
+  public constructor() {
+    this.registerRoutes();
+  }
+
+  private registerRoutes(): void {
+    this.router.post('/like', this.addLike);
+    this.router.delete('/like', this.removeLike);
+  }
+
+  private async addLike(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const likedVacation = new LikeModel(request.body);
+      await likesService.addLike(likedVacation);
+      response.status(StatusCode.Created);
+    } catch (err: any) {
+      next(err);
+    }
+  }
+
+  private async removeLike(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const likedVacation = new LikeModel(request.body);
+      await likesService.removeLike(likedVacation);
+      response.status(StatusCode.NoContent);
+    } catch (err: any) {
+      next(err);
+    }
+  }
+}
+
+const likesController = new LikesController();
+export const likesRouter = likesController.router;
