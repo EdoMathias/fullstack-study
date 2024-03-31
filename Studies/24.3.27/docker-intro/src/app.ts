d753a8ed7a6dd5502a589781bc0049590b99a6b1
Edo Mathias <edo.mathias@gmail.com>
2024-03-31 18:23:02 +0300

    Added docker compose commands

--- Studies/24.3.27/docker-intro/src/app.ts
+++ Studies/24.3.27/docker-intro/src/app.ts
@@ -6,17 +6,20 @@ dotenv.config();
 const server = express();
 
 server.get("/", (request: Request, response: Response) => {
-    response.send("<h1>Welcome to cute kittens REST API (by Assaf Finkelshtein)</h1>");
+  response.send("<h1>Welcome to Edo's first docker!</h1>");
 });
 
-server.get("/api/kittens", (request: Request, response: Response) => {
-    response.json(kittens);
+server.get("/api/edocker", (request: Request, response: Response) => {
+  response.json(kittens);
 });
 
-server.listen(process.env.PORT, () => console.log(`Listening on http://localhost:${process.env.PORT}`));
+server.listen(process.env.PORT, () =>
+  console.log(`Listening on http://localhost:${process.env.PORT}`)
+);
 
 const kittens = [
-    { id: 1, name: "Mitsi", age: 4 },
-    { id: 2, name: "Kitsi", age: 5 },
-    { id: 3, name: "Cyber", age: 8 }
+  { id: 1, name: "Mitsi", age: 4 },
+  { id: 2, name: "Kitsi", age: 5 },
+  { id: 3, name: "Cyber", age: 8 },
+  { id: 4, name: "Megatron", age: 1 },
 ];
