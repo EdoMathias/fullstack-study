c4706d17238b98f0f6bf794294ce7ed54653f885
Edo Mathias <edo.mathias@gmail.com>
2024-04-03 17:46:42 +0300

    Init front integration with docker

---
+++ Studies/24.3.31/rest-and-db/Frontend/src/Utils/AppConfig.ts
@@ -0,0 +1,12 @@
+class DevelopmentConfig {
+    public readonly productsUrl = "http://localhost:4000/api/products/";
+}
+
+class ProductionConfig {
+    public readonly productsUrl = "http://11.22.33.44:81/api/products/";
+}
+
+const appConfig = new DevelopmentConfig();
+// const appConfig = new ProductionConfig();
+
+export default appConfig;
