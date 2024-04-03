3151256668e9f072cca4af2898c51e1b94debafb
Edo Mathias <edo.mathias@gmail.com>
2024-04-03 19:23:33 +0300

    Edit all docker files to be ready for cloud deployment

--- Studies/24.3.31/rest-and-db/Frontend/src/Utils/AppConfig.ts
+++ Studies/24.3.31/rest-and-db/Frontend/src/Utils/AppConfig.ts
@@ -1,12 +1,12 @@
 class DevelopmentConfig {
-    public readonly productsUrl = "http://localhost:4000/api/products/";
+  public readonly productsUrl = 'http://localhost:81/api/products/';
 }
 
 class ProductionConfig {
-    public readonly productsUrl = "http://11.22.33.44:81/api/products/";
+  public readonly productsUrl = 'http://35.93.128.146:81/api/products/';
 }
 
-const appConfig = new DevelopmentConfig();
-// const appConfig = new ProductionConfig();
+// const appConfig = new DevelopmentConfig();
+const appConfig = new ProductionConfig();
 
 export default appConfig;
