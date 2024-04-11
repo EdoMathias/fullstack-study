aa0bea468495fb872a63ac965edb5a8698fb8196
EdoMathias <edo.mathias@overwolf.com>
2024-04-11 10:56:01 +0300

    Added authentications to vacations controller

--- Projects/project3/Edo Mathias/Backend/src/6-controllers/vacations-controller.ts
+++ Projects/project3/Edo Mathias/Backend/src/6-controllers/vacations-controller.ts
@@ -3,6 +3,7 @@ import { VacationModel } from '../3-models/vacation-model';
 import { vacationsService } from '../5-services/vacations-service';
 import { StatusCode } from '../3-models/status-codes';
 import { fileSaver } from 'uploaded-file-saver';
+import { securityMiddleware } from '../4-middleware/security-middleware';
 
 class VacationsController {
   public readonly router = express.Router();
@@ -12,18 +13,41 @@ class VacationsController {
   }
 
   private registerRoutes(): void {
-    this.router.get('/vacations-by-user/:userId', this.getAllVacations);
-    this.router.get('/vacations/:id', this.getVacationById);
-    this.router.put('/vacations/:id', this.editVacation);
-    this.router.delete('/vacations/:id', this.deleteVacation);
-    this.router.post('/vacations', this.addVacation);
+    this.router.get(
+      '/vacations-by-user/:userId',
+      securityMiddleware.verifyLoggedIn,
+      this.getAllVacations,
+    );
+    this.router.get(
+      '/vacations/:id',
+      securityMiddleware.verifyLoggedIn,
+      this.getVacationById,
+    );
+    this.router.post(
+      '/vacations',
+      securityMiddleware.verifyLoggedIn,
+      securityMiddleware.verifyAdmin,
+      this.addVacation,
+    );
+    this.router.put(
+      '/vacations/:id',
+      securityMiddleware.verifyLoggedIn,
+      securityMiddleware.verifyAdmin,
+      this.editVacation,
+    );
+    this.router.delete(
+      '/vacations/:id',
+      securityMiddleware.verifyLoggedIn,
+      securityMiddleware.verifyAdmin,
+      this.deleteVacation,
+    );
     this.router.get('/vacations/images/:imageName', this.getImageFile);
   }
 
   private async getAllVacations(
     request: Request,
     response: Response,
-    next: NextFunction
+    next: NextFunction,
   ): Promise<void> {
     try {
       const userId = +request.params.userId;
@@ -37,7 +61,7 @@ class VacationsController {
   private async getVacationById(
     request: Request,
     response: Response,
-    next: NextFunction
+    next: NextFunction,
   ): Promise<void> {
     try {
       const id = +request.params.id;
@@ -51,7 +75,7 @@ class VacationsController {
   private async getImageFile(
     request: Request,
     response: Response,
-    next: NextFunction
+    next: NextFunction,
   ): Promise<void> {
     try {
       const imageName = request.params.imageName;
@@ -65,7 +89,7 @@ class VacationsController {
   private async addVacation(
     request: Request,
     response: Response,
-    next: NextFunction
+    next: NextFunction,
   ): Promise<void> {
     try {
       request.body.image = request.files?.image;
@@ -82,7 +106,7 @@ class VacationsController {
   private async editVacation(
     request: Request,
     response: Response,
-    next: NextFunction
+    next: NextFunction,
   ): Promise<void> {
     try {
       request.body.image = request.files?.image;
@@ -98,7 +122,7 @@ class VacationsController {
   private async deleteVacation(
     request: Request,
     response: Response,
-    next: NextFunction
+    next: NextFunction,
   ): Promise<void> {
     try {
       const id = +request.params.id;
