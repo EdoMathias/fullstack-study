5bdc8c93eca22c2499219aeab51ec63156bf8ccb
EdoMathias <edo.mathias@overwolf.com>
2024-04-11 15:30:14 +0300

    Added a function to reset the state of vacations when for logout

--- Projects/project3/Edo Mathias/Frontend/src/Services/AuthService.ts
+++ Projects/project3/Edo Mathias/Frontend/src/Services/AuthService.ts
@@ -1,78 +1,74 @@
-import axios from "axios";
-import UserModel from "../Models/UserModel";
-import { appConfig } from "../Utils/AppConfig";
-import { jwtDecode } from "jwt-decode";
-import { appStore } from "../Redux/Store";
-import { authActionCreators } from "../Redux/AuthSlice";
-import CredentialsModel from "../Models/CredentialsModel";
+import axios from 'axios';
+import UserModel from '../Models/UserModel';
+import { appConfig } from '../Utils/AppConfig';
+import { jwtDecode } from 'jwt-decode';
+import { appStore } from '../Redux/Store';
+import { authActionCreators } from '../Redux/AuthSlice';
+import CredentialsModel from '../Models/CredentialsModel';
+import { vacationsActionCreators } from '../Redux/VacationsSlice';
 
 class AuthService {
+  // Constructor:
+  public constructor() {
+    // Get token from local storage:
+    const token = localStorage.getItem('token');
 
-    // Constructor:
-    public constructor() {
+    // If exists:
+    if (token) {
+      // Extract the user from the token:
+      const loggedInUser = jwtDecode<{ user: UserModel }>(token).user;
 
-        // Get token from local storage: 
-        const token = localStorage.getItem("token");
-
-        // If exists: 
-        if (token) {
-
-            // Extract the user from the token: 
-            const loggedInUser = jwtDecode<{ user: UserModel }>(token).user;
-
-            // Update global state: 
-            appStore.dispatch(authActionCreators.login(loggedInUser));
-        }
+      // Update global state:
+      appStore.dispatch(authActionCreators.login(loggedInUser));
     }
+  }
 
-    // Register a new user: 
-    public async register(user: UserModel): Promise<void> {
+  // Register a new user:
+  public async register(user: UserModel): Promise<void> {
+    // Send the new user to backend:
+    const response = await axios.post<string>(appConfig.registerUrl, user);
 
-        // Send the new user to backend: 
-        const response = await axios.post<string>(appConfig.registerUrl, user);
+    // Extract the JWT token:
+    const token = response.data;
 
-        // Extract the JWT token:
-        const token = response.data;
+    // Extract the user from the token:
+    const registeredUser = jwtDecode<{ user: UserModel }>(token).user;
 
-        // Extract the user from the token: 
-        const registeredUser = jwtDecode<{ user: UserModel }>(token).user;
+    // Update global state:
+    appStore.dispatch(authActionCreators.register(registeredUser));
 
-        // Update global state: 
-        appStore.dispatch(authActionCreators.register(registeredUser));
+    // Save the token in the local storage:
+    localStorage.setItem('token', token);
+  }
 
-        // Save the token in the local storage: 
-        localStorage.setItem("token", token);
-    }
+  // Login existing user:
+  public async login(credentials: CredentialsModel): Promise<void> {
+    // Send the credentials to backend:
+    const response = await axios.post<string>(appConfig.loginUrl, credentials);
 
-    // Login existing user:
-    public async login(credentials: CredentialsModel): Promise<void> {
+    // Extract the JWT token:
+    const token = response.data;
 
-        // Send the credentials to backend: 
-        const response = await axios.post<string>(appConfig.loginUrl, credentials);
+    // Extract the user from the token:
+    const loggedInUser = jwtDecode<{ user: UserModel }>(token).user;
 
-        // Extract the JWT token:
-        const token = response.data;
+    // Update global state:
+    appStore.dispatch(authActionCreators.login(loggedInUser));
 
-        // Extract the user from the token: 
-        const loggedInUser = jwtDecode<{ user: UserModel }>(token).user;
+    // Save the token in the local storage:
+    localStorage.setItem('token', token);
+  }
 
-        // Update global state: 
-        appStore.dispatch(authActionCreators.login(loggedInUser));
+  // Logout:
+  public logout(): void {
+    // Update global state:
+    appStore.dispatch(authActionCreators.logout());
 
-        // Save the token in the local storage: 
-        localStorage.setItem("token", token);
-    }
+    appStore.dispatch(vacationsActionCreators.resetState());
 
-    // Logout:
-    public logout(): void {
-
-        // Update global state: 
-        appStore.dispatch(authActionCreators.logout());
-
-        // Remove token from local storage: 
-        localStorage.removeItem("token");
-    }
-
+    // Remove token from local storage:
+    localStorage.removeItem('token');
+  }
 }
 
 export const authService = new AuthService();
