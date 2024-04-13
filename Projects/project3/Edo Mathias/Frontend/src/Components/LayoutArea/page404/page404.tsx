563d9d19c8baba2ba0ec42d6148581fd8bdadd2f
Edo Mathias <edo.mathias@gmail.com>
2024-04-13 18:41:04 +0300

    Some fixes to the 404 page

--- Projects/project3/Edo Mathias/Frontend/src/Components/LayoutArea/page404/page404.tsx
+++ Projects/project3/Edo Mathias/Frontend/src/Components/LayoutArea/page404/page404.tsx
@@ -1,15 +1,51 @@
-import "./page404.css";
+import { Button, ThemeProvider, createTheme } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import styles from './Page404.module.css';
 
 function Page404(): JSX.Element {
-    return (
-        <div className="page404">
+  const navigate = useNavigate();
 
-			<p>The page you are looking for doesn't exist.</p>
-
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/t3otBjVZzT0?autoplay=true" allow="autoplay" title="Page not Found"></iframe>
-
-        </div>
-    );
+  function navigateToVacationsList() {
+    navigate('/list');
+  }
+  return (
+    <ThemeProvider theme={page404Theme}>
+      <div className={styles.page404}>
+        <p>The page you are looking for doesn't exist.</p>
+        <Button size="large" onClick={navigateToVacationsList}>
+          Home
+        </Button>
+        <iframe
+          width="788"
+          height="443"
+          src="https://www.youtube.com/embed/-ZGlaAxB7nI"
+          title="YOU DIED (HD)"
+          allow="accelerometer; autoplay;"
+        ></iframe>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default Page404;
+
+const page404Theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          width: '10%',
+          marginBottom: '15px',
+          fontFamily: 'Mantinia Regular',
+          color: '#E2C799',
+          border: '1px solid #E2C799',
+          backgroundColor: '#3D405B',
+          '&:hover': {
+            backgroundColor: '#E2C799',
+            color: '#3D405B',
+          },
+        },
+      },
+    },
+  },
+});
