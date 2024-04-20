ad940814fdb3dae6f66a2193239d74e92112a6aa
Edo Mathias <edo.mathias@gmail.com>
2024-04-20 11:31:53 +0300

    Dockerized the backend and the sql

--- Projects/project3/Edo Mathias/Frontend/src/Utils/AppConfig.ts
+++ Projects/project3/Edo Mathias/Frontend/src/Utils/AppConfig.ts
@@ -1,4 +1,4 @@
-class AppConfig {
+class DevelopmentConfig {
   // Backend urls:
   public readonly vacationsUrl = 'http://localhost:4000/api/vacations/';
   public readonly vacationsByUserUrl =
@@ -16,4 +16,26 @@ class AppConfig {
   };
 }
 
-export const appConfig = new AppConfig();
+class ProductionConfig {
+  // Backend urls:
+  public readonly vacationsUrl = 'http://localhost:81/api/vacations/';
+  public readonly vacationsByUserUrl =
+    'http://localhost:81/api/vacations-by-user/';
+  public readonly registerUrl = 'http://localhost:81/api/register/';
+  public readonly loginUrl = 'http://localhost:81/api/login/';
+  public readonly likeUrl = 'http://localhost:81/api/like/';
+
+  //Axios options:
+  public readonly axiosOptions = {
+    headers: {
+      // Tell axios to also send the image:
+      'Content-Type': 'multipart/form-data', // We're sending also files.
+    },
+  };
+}
+
+// For local development:
+// export const appConfig = new DevelopmentConfig();
+
+// For docker:
+export const appConfig = new ProductionConfig();
